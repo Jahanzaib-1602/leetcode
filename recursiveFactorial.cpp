470b83b00def688ffbe0c40ff5bca5e9d1b6ea46
Jahanzaib <itx@gmail.com>
2024-09-19 19:22:55 +0500

    recursiveFactorial

--- recursiveFactorial.cpp
+++ recursiveFactorial.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+/*there are two pattern of recursion 
+one is head recursion and the other one is tail
+
+if base case is on the top and than recursive relation is written and at the last processing is done than it is the head recursion and if the base case is on the top and at the second number is processing and at the end there is a recursive relation than it is the tail recursion*/
 
 int factorial(int n){
     //base case 
