19db0f4c60350d20f4dc68ae7a35764b28b9f1b8
Jahanzaib <itx@gmail.com>
2025-01-23 01:43:44 +0500

    flattenLinkedList

---
+++ flattenLinkedList.cpp
@@ -0,0 +1,27 @@
+Node<int> merge(Node<int>* list1 ,Node<int>* list2){
+    Node<int>* t1 = list1;
+    Node<int>* t2 = list2;
+    Node<int>* dummyNode = new Node<int>(-1);
+    Node<int>* temp =  dummyNode;
+    while(t1 != NULL && t2 != NULL){
+        if(t1->val < t2->val){
+            temp->next = t1;
+            temp = t1;
+            t1 = t1->next;
+        }
+        else
+        {
+            temp->next = t2;
+            temp = t2;
+            t2 = t2->next;
+        }
+    }
+    if(t1)  temp->next = t1;
+    else temp->next = t2;
+    return dummyNode->next // it is actually a new head;
+}
+Node* flattenLinkedList(Node* head){
+    if(head == NULL || head->next == NULL return head;
+    Node* mergedHead = flattenLinkedList(head->next);
+    return merge(head, mergedHead);
+}
